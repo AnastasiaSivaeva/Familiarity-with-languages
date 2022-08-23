2802d88b568d899e8bac9661bbd0bfdd3906fa93
«Anastasia» <anastasia.koles@gmail.com>
2022-08-23 20:01:30 +0300

    решение 13 задачи

---
+++ Seminar_2/Task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру (слева направо) заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int a = num;
+if (a > 99)
+{
+    int res = (a / 100) % 10;
+    Console.WriteLine($"{res}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}

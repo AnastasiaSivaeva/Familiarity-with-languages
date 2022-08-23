a472e1ce824bce2e2e342b29b983e5f0ce99cc49
«Anastasia» <anastasia.koles@gmail.com>
2022-08-23 19:49:46 +0300

    решение 10 задачи

--- Seminar_2/Task_10/Program.cs
+++ Seminar_2/Task_10/Program.cs
@@ -4,6 +4,9 @@ Console.WriteLine("Введите трехзначное число: ");
 int num = int.Parse(Console.ReadLine());
 int a1 = num % 100;
 int a2 = num % 10;
-   
+
     int res = (a1 - a2) / 10;
     Console.WriteLine($"{res}");
+
+
+  

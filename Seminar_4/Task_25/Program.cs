b24106f22411972e6e7f10a4f2fd65cc267574af
«Anastasia» <anastasia.koles@gmail.com>
2022-08-31 20:19:15 +0300

    решение 25 задачи

---
+++ Seminar_4/Task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+int Expon(int numberA, int numberB)
+{
+   int result = 1;
+   for (int i = 1; i <= numberB; i++)
+   {
+    result = result * numberA;
+   }
+   return result;
+   
+}
+
+Console.WriteLine("Введите число A: ");
+int num_a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int num_b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{Expon(num_a, num_b)}");

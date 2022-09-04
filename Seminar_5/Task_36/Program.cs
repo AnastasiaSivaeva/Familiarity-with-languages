edb26093ddb358bf58d64e682003e281060b03b5
«Anastasia» <anastasia.koles@gmail.com>
2022-09-04 19:48:35 +0300

    решение 36 задачи

---
+++ Seminar_5/Task_36/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+   int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    { 
+      res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+int [] SumNumber(int[] array)
+{
+     int[] sum = new int[1];
+    for (int i = 1; i < array.Length; i++)
+    {
+       if (i % 2 != 0)
+       {
+          sum[0] += array[i];
+          i++;
+       }
+       else
+       {
+          i++;
+       }
+       
+    }
+    return sum;
+}
+int[] MyArray = GetArray(6, 1, 100);
+Console.WriteLine(String.Join(", ", MyArray));
+int[] result = SumNumber(MyArray);
+Console.WriteLine(String.Join(", ", result));

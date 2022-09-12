b3aef710b0f5c09c5abfb9fbd6fc22e1a971623f
«Anastasia» <anastasia.koles@gmail.com>
2022-09-12 20:12:47 +0300

    решение 41 задачи

---
+++ Seminar_6/Task_41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+int [] GetArray(int m)
+{
+   int[] res = new int[m];
+
+    for (int i = 0; i < res.Length; i++)
+    { 
+      Console.Write($"\nВведите элемент массива под индексом {i}: \t ");
+      res[i] = int.Parse(Console.ReadLine());
+    }
+    return res;
+}
+
+
+int PrintNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+       
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество элементов в массиве: ");
+int M = int.Parse(Console.ReadLine());
+int[] MyArray = GetArray(M);
+Console.WriteLine(String.Join(", ", MyArray));
+int result = PrintNumber(MyArray);
+Console.WriteLine(String.Join(", ", result));
---
+++ Seminar_6/Task_43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+
+double XCoord(int B1, int K1, int B2, int K2)
+{
+     double x = 0;
+    
+      x = (B1 - B2) / (K1 - K2);
+    return x;
+}
+
+double YCoord(int B1, int K1, double x1)
+{
+     double y = 0;
+    
+      y = (K1*x1) + B1;
+    return y;
+}
+
+
+
+
+Console.WriteLine("Введите значение b1: ");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+int k2 = int.Parse(Console.ReadLine());
+
+double result1 = XCoord(b1, k1, b2, k2);
+Console.WriteLine(String.Join("Точка пересечения, координата x; ", result1));
+double result2 = YCoord(b1, k1, result1);
+Console.WriteLine(String.Join("Точка пересечения, координата y; ", result2));

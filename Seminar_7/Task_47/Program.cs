3478a83754d81c01ac4cf4debbf5c4ace71f0156
«Anastasia» <anastasia.koles@gmail.com>
2022-09-13 20:42:38 +0300

    решение 47 задачи

---
+++ Seminar_7/Task_47/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+double[,] GetArray( int M, int N, int minValue, int maxValue)
+{
+    double[,] result = new double [M, N];
+
+    for (int i = 0; i < M; i++)
+    { 
+        for (int j = 0; j < N; j++)
+        {
+           result [i, j] = new Random().NextDouble() * minValue + maxValue;
+        }
+        
+    }
+    return result;
+}
+
+void PrintArray(double [,] arr2)
+{
+    for (int i = 0; i < arr2.GetLength(0); i++)
+    { 
+        for (int j = 0; j < arr2.GetLength(1); j++)
+        {
+           Console.Write($"{arr2[i, j]:f2} ");
+        }
+        Console.WriteLine();
+    }
+   
+}
+
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество стобцов: ");
+int n = int.Parse(Console.ReadLine());
+double[,] MyArray = GetArray(m, n, -10, 10);
+PrintArray(MyArray);
---
+++ Seminar_7/Task_50/Program.cs
@@ -0,0 +1,65 @@
+﻿// Напишите программу, которая на вход принимает число и генерирует случайный двумерный массив, и возвращает индексы этого элемента или же указание, что такого элемента нет
+// Console.WriteLine("Такого элемента нет")
+
+
+int[,] GetArray( int M, int N, int minValue, int maxValue)
+{
+    int[,] result = new int [M, N];
+
+    for (int i = 0; i < M; i++)
+    { 
+        for (int j = 0; j < N; j++)
+        {
+           result [i, j] = new Random().Next(minValue, maxValue);
+        }
+        
+    }
+    return result;
+}
+
+void PrintArray(int [,] arr2)
+{
+    for (int i = 0; i < arr2.GetLength(0); i++)
+    { 
+        for (int j = 0; j < arr2.GetLength(1); j++)
+        {
+           Console.Write($"{arr2[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+   
+}
+
+int IndNumber(int[,] arr3, int num)
+{
+     for (int i = 0; i < arr3.GetLength(0); i++)
+    { 
+        for (int j = 0; j < arr3.GetLength(1); j++)
+        {
+           if (num == arr3[i, j])
+           {
+            Console.WriteLine(String.Join("Индексы элемента  , ",i, j));
+           }
+           else
+           {
+            i++;
+            j++;
+           }
+        }
+        
+    }
+    return 0;
+}
+
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество стобцов: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+
+int[,] MyArray = GetArray(m, n, -10, 5);
+PrintArray(MyArray);
+int result = IndNumber(MyArray, number);
+Console.WriteLine(String.Join(", ",result));
